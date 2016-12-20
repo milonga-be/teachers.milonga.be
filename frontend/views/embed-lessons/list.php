79cb533b97dd02be476c0e1c29cabc0d700c4abf
Boris Verdeyen pazmiño <borispazmino@MacBook-Pro-de-Boris.local>
2016-12-20 17:25:40 +0100

    Adding http in website url if not present

--- frontend/views/embed-lessons/list.php
+++ frontend/views/embed-lessons/list.php
@@ -16,7 +16,7 @@ foreach($schools as $school){
 		<?= ( $school->phone )? '<a target="_blank" href="tel:' . $school->phone . '"><i class="fa fa-phone-square" aria-hidden="true"></i> ' . $school->phone . '</a> ':'' ?>
 		<?= ( $school->email )? '<a href="mailto:' . $school->email . '"><i class="fa fa-envelope" aria-hidden="true"></i> ' . $school->email . '</a><br>':'' ?>
 		<?= ( $school->facebook )? '<a target="_blank" href="' . $school->facebook . '"><i class="fa fa-facebook-square" aria-hidden="true"></i> On Facebook</a> ':'' ?>
-		<?= ( $school->website )? '<a target="_blank" href="' . $school->website . '"><i class="fa fa-globe" aria-hidden="true"></i> Website</a>':'' ?>
+		<?= ( $school->website )? '<a target="_blank" href="' . (( strpos($school->website,'http') === FALSE )? 'http://' . $school->website : $school->website) . '"><i class="fa fa-globe" aria-hidden="true"></i> Website</a>':'' ?>
 	</p>
 	<?php
 	foreach ($schools_venues[ $school->id ] as $venue) {
